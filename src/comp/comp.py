42be498027cf7148fbdcae88cddc6c88e5500f8d
Robert Kulp <pedalinon@gmail.com>
2020-07-17 22:31:51 -0700

    Oop 1 done, most of comprehensions done

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -63,13 +63,15 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [(name.name + name.age) for name in humans if name.age == 27 or name.age == 28 or name.age == 29 or name.age == 30 or name.age == 31 or name.age == 32 ]
+
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
+# g = [name.name.upper() + ', ' + name.age + 5 for name in humans]
 g = []
 print(g)
 
