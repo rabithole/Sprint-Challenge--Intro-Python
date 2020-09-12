913c78197fb9c91f71966b07a2a37ecfcd591913
Robert Kulp <pedalinon@gmail.com>
2020-09-11 19:19:56 -0700

    Stopping, I'm exhausted

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -21,11 +21,18 @@ humans = [
     Human("David", 31),
 ]
 
+print(humans[0].name[0] == 'A')
+
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [i.name for i in humans if i.name[0] == 'D']
 print(a)
+
+# for i in humans:
+#     if i.name[0] == 'D':
+#         print('')
+#         print(i.name)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
@@ -33,39 +40,39 @@ print("Ends with e:")
 b = []
 print(b)
 
-# Write a list comprehension that creates a list of names of everyone
-# whose name starts with any letter between 'C' and 'G' inclusive.
-print("Starts between C and G, inclusive:")
-c = []
-print(c)
+# # Write a list comprehension that creates a list of names of everyone
+# # whose name starts with any letter between 'C' and 'G' inclusive.
+# print("Starts between C and G, inclusive:")
+# c = []
+# print(c)
 
-# Write a list comprehension that creates a list of all the ages plus 10.
-print("Ages plus 10:")
-d = []
-print(d)
+# # Write a list comprehension that creates a list of all the ages plus 10.
+# print("Ages plus 10:")
+# d = []
+# print(d)
 
-# Write a list comprehension that creates a list of strings which are the name
-# joined to the age with a hyphen, for example "David-31", for all humans.
-print("Name hyphen age:")
-e = []
-print(e)
+# # Write a list comprehension that creates a list of strings which are the name
+# # joined to the age with a hyphen, for example "David-31", for all humans.
+# print("Name hyphen age:")
+# e = []
+# print(e)
 
-# Write a list comprehension that creates a list of tuples containing name and
-# age, for example ("David", 31), for everyone between the ages of 27 and 32,
-# inclusive.
-print("Names and ages between 27 and 32:")
-f = []
-print(f)
+# # Write a list comprehension that creates a list of tuples containing name and
+# # age, for example ("David", 31), for everyone between the ages of 27 and 32,
+# # inclusive.
+# print("Names and ages between 27 and 32:")
+# f = []
+# print(f)
 
-# Write a list comprehension that creates a list of new Humans like the old
-# list, except with all the names uppercase and the ages with 5 added to them.
-# The "humans" list should be unmodified.
-print("All names uppercase:")
-g = []
-print(g)
+# # Write a list comprehension that creates a list of new Humans like the old
+# # list, except with all the names uppercase and the ages with 5 added to them.
+# # The "humans" list should be unmodified.
+# print("All names uppercase:")
+# g = []
+# print(g)
 
-# Write a list comprehension that contains the square root of all the ages.
-print("Square root of ages:")
-import math
-h = []
-print(h)
+# # Write a list comprehension that contains the square root of all the ages.
+# print("Square root of ages:")
+# import math
+# h = []
+# print(h)
